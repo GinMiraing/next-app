e8c8a412adf71993bb9df77b9c4131c537323e8d
Gin Miraing <ginmiraing@outlook.com>
2024-02-09 22:46:32 +0800

    feat: 配置项更新

---
+++ lib/settings.ts
@@ -0,0 +1,5 @@
+export const BaseSetting = {
+  title: "胤·居",
+  avatar:
+    "https://cdn.zengjunyin.com/images/d29fe12c860c2b7a54db6483132fb0cf24d6aeb0.png",
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,19 @@
+import { isAxiosError } from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
+};
+
+export const formatError = (error: unknown) => {
+  if (isAxiosError(error)) {
+    return error.response?.data?.error;
+  } else if (error instanceof Error) {
+    return error.message;
+  } else {
+    return "unknown error";
+  }
 };
 
 export const sleep = (time: number) =>
